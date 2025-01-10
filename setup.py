85d98242b4e8d768afd949aa50869306ad09bd28
mayuk1607 <mayuk1607@gmail.com>
2025-01-10 20:26:02 +0000

    second commit

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_requirements(file_path:str)->List[str]:
 
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        [req.replace("\n","") for req in requirements]
+        requirements= [req.replace("\n","") for req in requirements]
 
         if hyphen_e_dot in requirements:
             requirements.remove(hyphen_e_dot)
